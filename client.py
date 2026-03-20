bd4ff5be6b0fdd4ec832850b3dc3efa84811aec1
Fulei Huang <fuleihuang99@gmail.com>
2026-03-19 23:38:35 -0400

    feat: Implement file transfer between client and server

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import asyncio
 import websockets
 
-async def client():
-    async with websockets.connect("ws://localhost:8080") as websocket:
-        await websocket.send("Hello server!")
-        response = await websocket.recv()
-        print("Server replied:", response)
+async def send_file():
+    uri = "ws://localhost:8000"
+    async with websockets.connect(uri) as ws:
+        with open("image.png", "rb") as f:  #open file in binary mode
+            file_bytes = f.read()
+        await ws.send(file_bytes)  # send bytes
+        response = await ws.recv()
+        print("Server response:", response)
 
-asyncio.run(client())
+asyncio.run(send_file())
--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 import asyncio # Python's built-in asynchronous I/O library
 import websockets # Library for creating WebSocket servers and clients
 
-async def handler(connection):
-    print("Client connected")
+# async def handler(connection):
+#     print("Client connected")
 
-    message = await connection.recv()
-    print("Received from client:", message)
-    await connection.send("Hello client!")
+#     message = await connection.recv()
+#     print("Received from client:", message)
+#     await connection.send("Hello client!")
+
+async def file_handler(ws):
+    print("Client connected, waiting for file...")
+    file_bytes = await ws.recv()  # receive bytes
+    with open("received_file.png", "wb") as f:
+        f.write(file_bytes)
+    print("File received and saved!")
+    await ws.send("File received successfully!")
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8080):
-        print("Server started on port 8080")
-        await asyncio.Future()
+    async with websockets.serve(file_handler, "localhost", 8000):
+        print("Server running on ws://localhost:8000")
+        await asyncio.sleep(50)  # keep server alive
 
 asyncio.run(main())
